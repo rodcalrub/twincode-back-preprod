1ee8518dcf8045b8c75153ab81d8b54b6d49c1d0
rodcalrub <rodcalrub@alum.us.es>
2021-10-13 12:16:06 +0200

    DONE analyze/show

--- scripts/render.r
+++ scripts/render.r
@@ -1,6 +1,7 @@
 
 print(.libPaths('D:/R-4.1.1/library')) #Quitar, sólo dev
 print(.libPaths())
+#Have to modify the method inside the node module to receive two parameters instead of one -> executeScript
 args <- commandArgs(trailingOnly = TRUE)
-print(args)
-rmarkdown::render('./scripts/test.Rmd', output_file = "./analysis/"+args+"/result.html", params=list(session = args))
+path <- paste("./analysis/",args,"/result.html",sep="")
+rmarkdown::render('./scripts/test.Rmd', output_file = path, params=list(session = args))
