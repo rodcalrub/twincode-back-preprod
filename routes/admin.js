ded77f864e642f58d39bc2afc22795d1b7b2b867
rodcalrub <rodcalrub@alum.us.es>
2021-11-17 22:44:13 +0100

    Tracking data generated from show

--- routes/admin.js
+++ routes/admin.js
@@ -1085,21 +1085,6 @@ router.get("/analyze/:sessionName", async (req, res) => {
 });
 
 
-function execShellCommand(cmd) {
-  const exec = require("child_process").exec;
-  return new Promise((resolve, reject) => {
-    exec(cmd, { maxBuffer: 1024 * 500 }, (error, stdout, stderr) => {
-      if (error) {
-        console.warn(error);
-      } else if (stdout) {
-        console.log(stdout);
-      } else {
-        console.log(stderr);
-      }
-      resolve(stdout ? true : false);
-    });
-  });
-}
 
 router.get("/analyze/:sessionName/show", async (req, res) => {
   const adminSecret = req.headers.authorization;
